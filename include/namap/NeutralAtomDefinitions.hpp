c2b72db3363fad7c8334e203f9ae132198495a58
Ludwig Schmid <ludwig.s.schmid@tum.de>
2024-01-20 13:56:04 +0100

    added doc strings to definitions.

--- include/namap/NeutralAtomDefinitions.hpp
+++ include/namap/NeutralAtomDefinitions.hpp
@@ -8,15 +8,24 @@
 #include "utils.hpp"
 
 namespace qc {
-using HwQubit      = uint32_t;
-using Swap         = std::pair<HwQubit, HwQubit>;
+// A CoordIndex corresponds to node in the SLM grid, where an atom can be placed (or not).
 using CoordIndex   = std::uint32_t;
 using CoordIndices = std::vector<CoordIndex>;
-using AtomMove     = std::pair<CoordIndex, CoordIndex>;
-using Qubits       = std::set<Qubit>;
+// A HwQubit corresponds to an atom in the neutral atom architecture. It can be used as qubit or not and occupies a
+// certain position in the architecture.
+using HwQubit      = uint32_t;
 using HwQubits     = std::set<HwQubit>;
-using HwPositions  = std::vector<HwQubits>;
-using SwapOrMove   = std::pair<HwQubit, HwQubit>;
+using HwPositions  = std::vector<HwQubits>; // TODO make consistent use between set and vector
+// A Qubit corresponds to a qubit in the quantum circuit. It can be mapped to a hardware qubit.
+using Qubits       = std::set<Qubit>;
+
+// Swaps are between hardware Qubits (one of them can be unmapped).
+using Swap         = std::pair<HwQubit, HwQubit>;
+// Moves are between coordinates (the first is occupied, the second is not).
+using AtomMove     = std::pair<CoordIndex, CoordIndex>;
+using SwapOrMove   = std::pair<HwQubit, HwQubit>; // TODO remove unclear naming
+
+// Used to represent operations
 using OpPointer    = std::unique_ptr<AodOperation>;
 
 } // namespace qc
