55135d06982c586e0da0bd0d9fec5503c42f56e1
Ludwig Schmid <ludwig.s.schmid@tum.de>
2024-01-20 12:45:02 +0100

    added simple tests

--- test/namap/test_namap.cpp
+++ test/namap/test_namap.cpp
@@ -8,41 +8,98 @@
 #include "namap/NeutralAtomScheduler.hpp"
 
 #include "gtest/gtest.h"
+#include <string>
 
-TEST(NeutralAtomTest, architcure) {
-  std::filesystem::path const testDirPath = std::filesystem::current_path();
-  std::string const           testDir     = testDirPath.string();
-  auto filename                    = std::string(testDir) + "/rubidium.json";
-  qc::NeutralAtomArchitecture arch = qc::NeutralAtomArchitecture(filename);
-  EXPECT_EQ(arch.getCoordinate(0).getX(), 0);
-  EXPECT_EQ(arch.getNcolumns(), 5);
-  //  auto d = arch.getDistance(0, 1);
-  auto d =
-      arch.getEuclidianDistance(arch.getCoordinate(0), arch.getCoordinate(1));
-  EXPECT_EQ(d, 1);
-  EXPECT_EQ(arch.getSwapDistance(0, 1), 0);
-  EXPECT_EQ(arch.getSwapDistance(0, 2), 0);
-  EXPECT_EQ(arch.getSwapDistance(0, 3), 1);
+class NeutralAtomArchitectureTest
+    : public ::testing::TestWithParam<std::string> {
+protected:
+  std::string testArchitecturePath = "../../test/namap/architectures/";
+
+  void SetUp() override { testArchitecturePath += GetParam() + ".json"; }
+};
+
+TEST_P(NeutralAtomArchitectureTest, LoadArchitecures) {
+  auto arch = qc::NeutralAtomArchitecture(testArchitecturePath);
+
+  // Test get properties
+  EXPECT_LE(arch.getNqubits(), arch.getNpositions());
+  EXPECT_EQ(arch.getNpositions(), arch.getNrows() * arch.getNcolumns());
+  // Test precomputed values
+  auto c1 = arch.getCoordinate(0);
+  auto c2 = arch.getCoordinate(1);
+  EXPECT_GE(arch.getSwapDistance(c1, c2), 0);
+  EXPECT_GE(arch.getNAodIntermediateLevels(), 1);
+  // Test get parameters
+  EXPECT_GE(arch.getGateTime("cz"), 0);
+  EXPECT_GE(arch.getGateAverageFidelity("cz"), 0);
+  // Test distance functions
+  EXPECT_GE(arch.getEuclidianDistance(c1, c2), 0);
+  // Test MoveVector functions
+  auto mv = arch.getVector(0, 1);
+  EXPECT_GE(arch.getVectorShuttlingTime(mv), 0);
 }
 
-TEST(NeutralAtomTest, mapper) {
-  std::filesystem::path const testDirPath = std::filesystem::current_path();
-  std::string const           testDir     = testDirPath.string();
-  auto filename                    = std::string(testDir) + "/rubidium.json";
-  qc::NeutralAtomArchitecture arch = qc::NeutralAtomArchitecture(filename);
-  qc::NeutralAtomMapper       mapper =
-      qc::NeutralAtomMapper(arch, static_cast<qc::InitialCoordinateMapping>(0));
-  //    auto qc =
-  //    qc::QuantumComputation("/Users/lude/Downloads/portfoliovqe_nativegates_ibm_qiskit_opt3_15.qasm");
-  //  auto qc = qc::QuantumComputation("../../examples/qft_10.qasm");
-  auto qc = qc::QuantumComputation("../../examples/qft_16.qasm");
-  //  auto qc = qc::QuantumComputation("../../examples/test.qasm");
+INSTANTIATE_TEST_SUITE_P(NeutralAtomArchitectureTestSuite,
+                         NeutralAtomArchitectureTest,
+                         ::testing::Values("rubidium", "rubidium_hybrid",
+                                           "rubidium_shuttling"));
+class NeutralAtomMapperTest
+    // parameters are architecture, circuit, gateWeight, shuttlingWeight,
+    // lookAheadWeight
+    : public ::testing::TestWithParam<
+          std::tuple<std::string, std::string, qc::fp, qc::fp, qc::fp>> {
+protected:
+  std::string testArchitecturePath = "../../test/namap/architectures/";
+  std::string testQcPath           = "../../test/namap/circuits/";
+  qc::fp      gateWeight           = 1;
+  qc::fp      shuttlingWeight      = 1;
+  qc::fp      lookAheadWeight      = 1;
+  // fixed
+  qc::fp decay               = 0.1;
+  qc::fp shuttlingTimeWeight = 0.1;
 
-  auto qcMapped = mapper.map(qc);
+  void SetUp() override {
+    auto params = GetParam();
+    testArchitecturePath += std::get<0>(params) + ".json";
+    testQcPath += std::get<1>(params) + ".qasm";
+    gateWeight      = std::get<2>(params);
+    shuttlingWeight = std::get<3>(params);
+    lookAheadWeight = std::get<4>(params);
+  }
+};
 
-  qc::NeutralAtomScheduler scheduler = qc::NeutralAtomScheduler(
-      arch, static_cast<qc::InitialCoordinateMapping>(0));
-  scheduler.schedule(qcMapped, false);
+TEST_P(NeutralAtomMapperTest, MapCircuitsIdentity) {
+  auto               arch = qc::NeutralAtomArchitecture(testArchitecturePath);
+  qc::InitialMapping initialMapping = qc::InitialMapping::Identity;
+  qc::InitialCoordinateMapping initialCoordinateMapping =
+      qc::InitialCoordinateMapping::Trivial;
+  qc::NeutralAtomMapper mapper(arch, initialCoordinateMapping);
+  qc::MapperParameters  mapperParameters;
+  mapperParameters.lookaheadWeightSwaps = lookAheadWeight;
+  mapperParameters.lookaheadWeightMoves = lookAheadWeight;
+  mapperParameters.decay                = decay;
+  mapperParameters.shuttlingTimeWeight  = shuttlingTimeWeight;
+  mapperParameters.gateWeight           = gateWeight;
+  mapperParameters.shuttlingWeight      = shuttlingWeight;
+  mapper.setParameters(mapperParameters);
 
-  EXPECT_EQ(0, 0);
+  qc::QuantumComputation qc(testQcPath);
+  auto                   qcMapped = mapper.map(qc, initialMapping, false);
+
+  qc::NeutralAtomScheduler scheduler(arch);
+  auto scheduleResults = scheduler.schedule(qcMapped, false);
+
+  ASSERT_GT(scheduleResults.totalFidelities, 0);
+  ASSERT_GT(scheduleResults.totalIdleTime, 0);
+  ASSERT_GT(scheduleResults.totalExecutionTime, 0);
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    NeutralAtomMapperTestSuite, NeutralAtomMapperTest,
+    ::testing::Combine(
+        ::testing::Values("rubidium", "rubidium_hybrid", "rubidium_shuttling"),
+        ::testing::Values("dj_nativegates_rigetti_qiskit_opt3_10", "modulo_2",
+                          "multiply_2",
+                          "qft_nativegates_rigetti_qiskit_opt3_10"),
+        ::testing::Values(1, 0.), ::testing::Values(1, 0.),
+        ::testing::Values(0, 0.1)));
