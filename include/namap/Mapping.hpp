f92baf47fb3b5b4bc54b3d4dfcdc30d0b72a6bc8
Ludwig Schmid <ludwig.s.schmid@tum.de>
2024-01-20 16:02:53 +0100

    added doc strings to Mapping.hpp

--- include/namap/Mapping.hpp
+++ include/namap/Mapping.hpp
@@ -9,8 +9,10 @@
 
 namespace qc {
 
-// class to manage the mapping between circuit qubits and hardware qubits
-// in a bijective manner
+/**
+ * @brief Class to manage the mapping between circuit qubits and hardware qubits
+ * in a bijective manner.
+ */
 class Mapping {
 protected:
   // std::map<Qubit, HwQubit>
@@ -27,14 +29,29 @@ public:
       break;
     }
   }
+  /**
+   * @brief Assigns a circuit qubit to a hardware qubit.
+   * @param qubit The circuit qubit to be assigned
+   * @param hwQubit The hardware qubit to be assigned
+   */
   void inline setCircuitQubit(Qubit qubit, HwQubit hwQubit) {
     circToHw[qubit] = hwQubit;
   }
 
+  /**
+   * @brief Returns the hardware qubit assigned to the given circuit qubit.
+   * @param qubit The circuit qubit to be queried
+   * @return The hardware qubit assigned to the given circuit qubit
+   */
   [[nodiscard]] inline HwQubit getHwQubit(Qubit qubit) const {
     return circToHw.at(qubit);
   }
 
+  /**
+   * @brief Returns the hardware qubits assigned to the given circuit qubits.
+   * @param qubits The circuit qubits to be queried
+   * @return The hardware qubits assigned to the given circuit qubits
+   */
   [[nodiscard]] inline std::set<HwQubit>
   getHwQubits(std::set<Qubit>& qubits) const {
     std::set<HwQubit> hwQubits;
@@ -44,6 +61,13 @@ public:
     return hwQubits;
   }
 
+  /**
+   * @brief Returns the circuit qubit assigned to the given hardware qubit.
+   * @details Throws an exception if the hardware qubit is not assigned to any
+   * circuit qubit.
+   * @param qubit The hardware qubit to be queried
+   * @return The circuit qubit assigned to the given hardware qubit
+   */
   [[nodiscard]] inline Qubit getCircQubit(HwQubit qubit) const {
     for (const auto& [circQubit, hwQubit] : circToHw) {
       if (hwQubit == qubit) {
@@ -54,17 +78,34 @@ public:
                              " not found in mapping");
   }
 
+  /**
+   * @brief Indicates if any circuit qubit is assigned to the given hardware
+   * qubit.
+   * @param qubit The hardware qubit to be queried
+   * @return True if any circuit qubit is assigned to the given hardware qubit,
+   * false otherwise
+   */
   [[nodiscard]] inline bool isMapped(HwQubit qubit) const {
     return std::any_of(
         circToHw.begin(), circToHw.end(),
         [qubit](const auto& pair) { return pair.second == qubit; });
   }
 
+  /**
+   * @brief Converts the qubits of an operation from circuit qubits to hardware
+   * qubits.
+   * @param op The operation to be converted
+   */
   inline void mapToHwQubits(Operation* op) const {
     op->setTargets(circToHw.apply(op->getTargets()));
     op->setControls(circToHw.apply(op->getControls()));
   }
 
+  /**
+   * @brief Interchanges the mapping of two hardware qubits. At least one of it must be
+   * mapped to a circuit qubit.
+   * @param swap The two circuit qubits to be swapped
+   */
   void swap(Swap swap);
 };
 
